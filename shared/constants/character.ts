f2f3cff9828971d1c058d55aa409238a4a5a18a1
AlvISsReimu <alvissreimu@gmail.com>
2024-10-25 06:40:01 -0700

    feat: add some characters (half done)

--- shared/constants/character.ts
+++ shared/constants/character.ts
@@ -53,4 +53,130 @@ export const ALL_CHARACTERS: Character[] = [
       new ExtraLine(4, [], [createElementLimitedSlot(ElementType.Fire, 2)]),
     ),
   },
+  {
+    id: 3,
+    name_i18n: {
+      [LanguageCode.ZH_CN]: '亚伦',
+      [LanguageCode.JA]: 'アーロン',
+      [LanguageCode.EN]: 'Aaron',
+    },
+    core: new Core(
+      new WeaponLine(4, [], [createElementLimitedSlot(ElementType.Wind, 1)]),
+      new ShieldLine(4, [], [createElementLimitedSlot(ElementType.Fire, 2)]),
+      new DriveLine(3, [], []),
+      new ExtraLine(4, [], [createElementLimitedSlot(ElementType.Wind, 2)]),
+    ),
+  },
+  {
+    id: 4,
+    name_i18n: {
+      [LanguageCode.ZH_CN]: '莉婕特',
+      [LanguageCode.JA]: 'リゼット',
+      [LanguageCode.EN]: 'Risette',
+    },
+    core: new Core(
+      new WeaponLine(4, [], []),
+      new ShieldLine(4, [], [createElementLimitedSlot(ElementType.Water, 1)]),
+      new DriveLine(4, [], [createElementLimitedSlot(ElementType.Wind, 2)]),
+      new ExtraLine(4, [], [createElementLimitedSlot(ElementType.Water, 1)]),
+    ),
+  },
+  {
+    id: 5,
+    name_i18n: {
+      [LanguageCode.ZH_CN]: '卡特尔',
+      [LanguageCode.JA]: 'カトル',
+      [LanguageCode.EN]: 'Quatre',
+    },
+    core: new Core(
+      new WeaponLine(4, [], [createElementLimitedSlot(ElementType.Earth, 2)]),
+      new ShieldLine(4, [], [createElementLimitedSlot(ElementType.Earth, 1)]),
+      new DriveLine(4, [], [createElementLimitedSlot(ElementType.Space, 2)]),
+      new ExtraLine(3, [], []),
+    ),
+  },
+  {
+    id: 6,
+    name_i18n: {
+      [LanguageCode.ZH_CN]: '茱迪丝',
+      [LanguageCode.JA]: 'ジュディス',
+      [LanguageCode.EN]: 'Judith',
+    },
+    core: new Core(
+      new WeaponLine(4, [], [createElementLimitedSlot(ElementType.Time, 2)]),
+      new ShieldLine(3, [], []),
+      new DriveLine(4, [], [createElementLimitedSlot(ElementType.Mirage, 2)]),
+      new ExtraLine(4, [], [createElementLimitedSlot(ElementType.Mirage, 1)]),
+    ),
+  },
+  {
+    id: 7,
+    name_i18n: {
+      [LanguageCode.ZH_CN]: '贝尔加德',
+      [LanguageCode.JA]: 'ベルガルド',
+      [LanguageCode.EN]: 'Bergard',
+    },
+    core: new Core(
+      new WeaponLine(4, [], [createElementLimitedSlot(ElementType.Space, 1)]),
+      new ShieldLine(4, [], [createElementLimitedSlot(ElementType.Earth, 2)]),
+      new DriveLine(4, [], [createElementLimitedSlot(ElementType.Space, 2)]),
+      new ExtraLine(3, [], []),
+    ),
+  },
+  {
+    id: 8,
+    name_i18n: {
+      [LanguageCode.ZH_CN]: '艾蕾因',
+      [LanguageCode.JA]: 'エレイン',
+      [LanguageCode.EN]: 'Elaine',
+    },
+    core: new Core(
+      new WeaponLine(4, [], [createElementLimitedSlot(ElementType.Mirage, 1)]),
+      new ShieldLine(4, [], [createElementLimitedSlot(ElementType.Wind, 2)]),
+      new DriveLine(4, [], [createElementLimitedSlot(ElementType.Mirage, 2)]),
+      new ExtraLine(3, [], []),
+    ),
+  },
+  {
+    id: 9,
+    name_i18n: {
+      [LanguageCode.ZH_CN]: '静名',
+      [LanguageCode.JA]: 'シズナ',
+      [LanguageCode.EN]: 'Shizuna',
+    },
+    core: new Core(
+      new WeaponLine(4, [], [createElementLimitedSlot(ElementType.Water, 2)]),
+      new ShieldLine(4, [], [createElementLimitedSlot(ElementType.Time, 2)]),
+      new DriveLine(4, [], []),
+      new ExtraLine(4, [], [createElementLimitedSlot(ElementType.Water, 1)]),
+    ),
+  },
+  {
+    id: 10,
+    name_i18n: {
+      [LanguageCode.ZH_CN]: '蕾恩',
+      [LanguageCode.JA]: 'レン',
+      [LanguageCode.EN]: 'Renne',
+    },
+    core: new Core(
+      new WeaponLine(4, [], [createElementLimitedSlot(ElementType.Space, 2)]),
+      new ShieldLine(4, [], []),
+      new DriveLine(4, [], [createElementLimitedSlot(ElementType.Mirage, 1)]),
+      new ExtraLine(4, [], [createElementLimitedSlot(ElementType.Mirage, 2)]),
+    ),
+  },
+  {
+    id: 11,
+    name_i18n: {
+      [LanguageCode.ZH_CN]: '约尔妲',
+      [LanguageCode.JA]: 'ヨルダ',
+      [LanguageCode.EN]: 'Jorda',
+    },
+    core: new Core(
+      new WeaponLine(3, [], []),
+      new ShieldLine(4, [], [createElementLimitedSlot(ElementType.Time, 2)]),
+      new DriveLine(4, [], [createElementLimitedSlot(ElementType.Time, 1)]),
+      new ExtraLine(4, [], [createElementLimitedSlot(ElementType.Fire, 2)]),
+    ),
+  },
 ] as Character[]
