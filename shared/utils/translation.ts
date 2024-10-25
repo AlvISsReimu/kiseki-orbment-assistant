6158641471a5906f4248fd6903124a5cd92d5a4f
wenjun li <li407017533@gmail.com>
2024-10-25 00:32:12 -0700

    fix: add missing file

---
+++ shared/utils/translation.ts
@@ -0,0 +1,28 @@
+import { Language } from "@shared/enums/language";
+
+export const translation = {
+  global: {
+    language: {
+      [Language.ZH_CN]: '语言',
+      [Language.JA]: '言語',
+      [Language.EN]: 'Language',
+    },
+    chinese: {
+      [Language.ZH_CN]: '简体中文',
+      // chinese in japanese
+      [Language.JA]: '簡体字',
+      // chinese in english
+      [Language.EN]: 'Simplified Chinese',
+    },
+    japanese: {
+      [Language.ZH_CN]: '日语',
+      [Language.JA]: '日本語',
+      [Language.EN]: 'Japanese',
+    },
+    english: {
+      [Language.ZH_CN]: '英语',
+      [Language.JA]: '英語',
+      [Language.EN]: 'English',
+    },
+  }
+}
