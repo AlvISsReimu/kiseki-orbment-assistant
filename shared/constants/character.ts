13b866c3072779378b6a433831c7718b41dfa376
AlvISsReimu <alvissreimu@gmail.com>
2024-10-25 16:50:44 -0700

    feat: finish all character const

--- shared/constants/character.ts
+++ shared/constants/character.ts
@@ -179,4 +179,172 @@ export const ALL_CHARACTERS: Character[] = [
       new ExtraLine(4, [], [createElementLimitedSlot(ElementType.Fire, 2)]),
     ),
   },
+  {
+    id: 12,
+    name_i18n: {
+      [LanguageCode.ZH_CN]: '黎恩',
+      [LanguageCode.JA]: 'リィン',
+      [LanguageCode.EN]: 'Rean',
+    },
+    core: new Core(
+      new WeaponLine(4, [], [createElementLimitedSlot(ElementType.Fire, 1)]),
+      new ShieldLine(4, [], [createElementLimitedSlot(ElementType.Fire, 2)]),
+      new DriveLine(3, [], []),
+      new ExtraLine(4, [], [createElementLimitedSlot(ElementType.Time, 2)]),
+    ),
+  },
+  {
+    id: 13,
+    name_i18n: {
+      [LanguageCode.ZH_CN]: '亚尔缇娜',
+      [LanguageCode.JA]: 'アルティナ',
+      [LanguageCode.EN]: 'Altina',
+    },
+    core: new Core(
+      new WeaponLine(3, [], []),
+      new ShieldLine(4, [], [createElementLimitedSlot(ElementType.Water, 2)]),
+      new DriveLine(4, [], [createElementLimitedSlot(ElementType.Mirage, 1)]),
+      new ExtraLine(4, [], [createElementLimitedSlot(ElementType.Mirage, 2)]),
+    ),
+  },
+  {
+    id: 14,
+    name_i18n: {
+      [LanguageCode.ZH_CN]: '菲',
+      [LanguageCode.JA]: 'フィー',
+      [LanguageCode.EN]: 'Fie',
+    },
+    core: new Core(
+      new WeaponLine(4, [], [createElementLimitedSlot(ElementType.Wind, 1)]),
+      new ShieldLine(4, [], [createElementLimitedSlot(ElementType.Wind, 2)]),
+      new DriveLine(3, [], []),
+      new ExtraLine(4, [], [createElementLimitedSlot(ElementType.Time, 2)]),
+    ),
+  },
+  {
+    id: 15,
+    name_i18n: {
+      [LanguageCode.ZH_CN]: '克洛',
+      [LanguageCode.JA]: 'クロウ',
+      [LanguageCode.EN]: 'Crow',
+    },
+    core: new Core(
+      new WeaponLine(4, [], [createElementLimitedSlot(ElementType.Wind, 1)]),
+      new ShieldLine(4, [], [createElementLimitedSlot(ElementType.Wind, 2)]),
+      new DriveLine(3, [], []),
+      new ExtraLine(4, [], [createElementLimitedSlot(ElementType.Time, 2)]),
+    ),
+  },
+  {
+    id: 16,
+    name_i18n: {
+      [LanguageCode.ZH_CN]: '塞莉丝',
+      [LanguageCode.JA]: 'セリス',
+      [LanguageCode.EN]: 'Celis',
+    },
+    core: new Core(
+      new WeaponLine(4, [], [createElementLimitedSlot(ElementType.Fire, 1)]),
+      new ShieldLine(3, [], [createElementLimitedSlot(ElementType.Fire, 2)]),
+      new DriveLine(4, [], []),
+      new ExtraLine(4, [], [createElementLimitedSlot(ElementType.Earth, 2)]),
+    ),
+  },
+  {
+    id: 17,
+    name_i18n: {
+      [LanguageCode.ZH_CN]: '里翁',
+      [LanguageCode.JA]: 'リオン',
+      [LanguageCode.EN]: 'Leon',
+    },
+    core: new Core(
+      new WeaponLine(4, [], [createElementLimitedSlot(ElementType.Water, 2)]),
+      new ShieldLine(4, [], [createElementLimitedSlot(ElementType.Space, 2)]),
+      new DriveLine(4, [], [createElementLimitedSlot(ElementType.Water, 1)]),
+      new ExtraLine(3, [], []),
+    ),
+  },
+  {
+    id: 18,
+    name_i18n: {
+      [LanguageCode.ZH_CN]: '金',
+      [LanguageCode.JA]: 'ジン',
+      [LanguageCode.EN]: 'Zin',
+    },
+    core: new Core(
+      new WeaponLine(4, [], [createElementLimitedSlot(ElementType.Earth, 2)]),
+      new ShieldLine(4, [], [createElementLimitedSlot(ElementType.Earth, 1)]),
+      new DriveLine(3, [], []),
+      new ExtraLine(4, [], [createElementLimitedSlot(ElementType.Water, 2)]),
+    ),
+  },
+  {
+    id: 19,
+    name_i18n: {
+      [LanguageCode.ZH_CN]: '凯文',
+      [LanguageCode.JA]: 'ケビン',
+      [LanguageCode.EN]: 'Kevin',
+    },
+    core: new Core(
+      new WeaponLine(3, [], [createElementLimitedSlot(ElementType.Space, 2)]),
+      new ShieldLine(4, [], [createElementLimitedSlot(ElementType.Time, 1)]),
+      new DriveLine(4, [], []),
+      new ExtraLine(4, [], [createElementLimitedSlot(ElementType.Space, 2)]),
+    ),
+  },
+  {
+    id: 20,
+    name_i18n: {
+      [LanguageCode.ZH_CN]: '卢法斯',
+      [LanguageCode.JA]: 'ルーファス',
+      [LanguageCode.EN]: 'Rufus',
+    },
+    core: new Core(
+      new WeaponLine(4, [], [createElementLimitedSlot(ElementType.Space, 2)]),
+      new ShieldLine(3, [], []),
+      new DriveLine(4, [], [createElementLimitedSlot(ElementType.Wind, 2)]),
+      new ExtraLine(4, [], [createElementLimitedSlot(ElementType.Wind, 1)]),
+    ),
+  },
+  {
+    id: 21,
+    name_i18n: {
+      [LanguageCode.ZH_CN]: '斯温',
+      [LanguageCode.JA]: 'スウィン',
+      [LanguageCode.EN]: 'Swin',
+    },
+    core: new Core(
+      new WeaponLine(4, [], [createElementLimitedSlot(ElementType.Wind, 2)]),
+      new ShieldLine(4, [], [createElementLimitedSlot(ElementType.Time, 1)]),
+      new DriveLine(3, [], []),
+      new ExtraLine(4, [], [createElementLimitedSlot(ElementType.Time, 2)]),
+    ),
+  },
+  {
+    id: 22,
+    name_i18n: {
+      [LanguageCode.ZH_CN]: '娜狄雅',
+      [LanguageCode.JA]: 'ナーディア',
+      [LanguageCode.EN]: 'Nadia',
+    },
+    core: new Core(
+      new WeaponLine(4, [], [createElementLimitedSlot(ElementType.Fire, 2)]),
+      new ShieldLine(4, [], []),
+      new DriveLine(4, [], [createElementLimitedSlot(ElementType.Fire, 1)]),
+      new ExtraLine(4, [], [createElementLimitedSlot(ElementType.Water, 2)]),
+    ),
+  },
+  {
+    id: 23,
+    name_i18n: {
+      [LanguageCode.ZH_CN]: '莉夏',
+      [LanguageCode.JA]: 'リーシャ',
+      [LanguageCode.EN]: 'Rixia',
+    },
+    core: new Core(
+      new WeaponLine(4, [], [createElementLimitedSlot(ElementType.Mirage, 1)]),
+      new ShieldLine(3, [], []),
+      new DriveLine(4, [], [createElementLimitedSlot(ElementType.Fire, 2)]),
+      new ExtraLine(4, [], [createElementLimitedSlot(ElementType.Mirage, 2)]),
+    ),
+  },
 ] as Character[]
