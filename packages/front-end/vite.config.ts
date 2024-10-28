b19c455261c5c99ae164be8b6a43c0745c0e24b3
AlvISsReimu <alvissreimu@gmail.com>
2024-10-27 21:18:51 -0700

    chore: use manual chunks to split huge js into multiple

--- packages/front-end/vite.config.ts
+++ packages/front-end/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
@@ -12,6 +12,18 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: path.resolve(__dirname, 'index.html'),
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString()
+          }
+        },
+        assetFileNames: 'assets/[name].[hash][extname]',
+      },
     },
   },
 })
